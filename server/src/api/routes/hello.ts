705a6f7fa4a35a6bcf9345511c82da6e2620a9b9
Maurits Arissen <maurits.arissen@student.hu.nl>
2021-11-04 13:07:44 +0100

    Changed last commit back, wasn't worth it

--- server/src/api/routes/hello.ts
+++ server/src/api/routes/hello.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response, Router } from "express";
 const route = Router();
 
 export default (app: Router) => {
-  app.use("/hello_world", route);
+  app.use("/helloworld", route);
 
   route.get("/", async (req: Request, res: Response, next: NextFunction) => {
     return res.json({ message: "Hello world!" }).status(200);
