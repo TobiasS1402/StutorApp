b0ea38a5121991894cefe34c03b15ccdc5fa8046
Maurits Arissen <maurits.arissen@student.hu.nl>
2021-11-04 13:09:30 +0100

    added _ to endpoint

--- server/src/api/routes/hello.ts
+++ server/src/api/routes/hello.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response, Router } from "express";
 const route = Router();
 
 export default (app: Router) => {
-  app.use("/helloworld", route);
+  app.use("/hello_world", route);
 
   route.get("/", async (req: Request, res: Response, next: NextFunction) => {
     return res.json({ message: "Hello world!" }).status(200);
