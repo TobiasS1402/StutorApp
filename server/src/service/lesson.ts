88ac7df5f9954a7ce7362d1cb95ad5018755d46b
Maurits <maurits.arissen@student.hu.nl>
2021-12-09 13:40:25 +0100

    Update lesson.ts

--- server/src/service/lesson.ts
+++ server/src/service/lesson.ts
@@ -41,7 +41,7 @@ export default class LessonService {
             "avgRating",
           ],
         ],
-        group: ["Lesson._id"],
+        group: ["Lesson._id", "user._id"],
         include: this.fullLessonIncludes(),
       });
       if (!lessonRecord) throw new CustomError(responses.LESSON_NOT_FOUND);
