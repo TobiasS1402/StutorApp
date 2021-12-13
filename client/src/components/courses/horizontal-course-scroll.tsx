c09e645e1823ce4287afb64f42d148d91329403b
bartvt21 <bart.vantongeren@student.hu.nl>
2021-12-13 13:34:16 +0100

    added courses api to home

--- client/src/components/courses/horizontal-course-scroll.tsx
+++ client/src/components/courses/horizontal-course-scroll.tsx
@@ -1,42 +1,19 @@
 import { CourseCard } from '@components/cards'
 import * as React from 'react'
 import { ScrollView } from 'react-native'
-
-// dummy data
-const courses = [
-  {
-    id: 0,
-    name: 'Software Architecture',
-    icon: 'software-architecture',
-  },
-  {
-    id: 1,
-    name: 'Advanced Software design',
-    icon: 'software-architecture',
-  },
-  {
-    id: 2,
-    name: 'Object oriented analysis',
-    icon: 'software-architecture',
-  },
-  {
-    id: 3,
-    name: 'Patterns & Framework',
-    icon: 'software-architecture',
-  },
-  {
-    id: 4,
-    name: 'Programming',
-    icon: 'software-architecture',
-  },
-]
+import coursesApi from '@/api/coursesApi'
+import { useApi } from '@/hooks/useApi'
+import { ICourse } from '@/types'
 
 export const HorizontalCourseScroll = () => {
+  const service = useApi<ICourse[]>(coursesApi.getCourses)
+
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {courses.map((item) => (
-        <CourseCard key={item.id} name={item.name} />
-      ))}
+      {service.status === 'loaded' &&
+        service.payload['courses'].map((item: ICourse) => (
+          <CourseCard key={item._id} name={item.name} />
+        ))}
     </ScrollView>
   )
 }
